f644f77d2a9801e94a69ead593c115bb7e23ccdc
DESKTOP-EB30IMJ\霍青娥 <1520530570@qq.com>
2025-09-27 15:07:20 +0800

    no message

--- src/stores/nav.ts
+++ src/stores/nav.ts
@@ -3,7 +3,7 @@ import { ref,watch } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useNavStore = defineStore('nav', () => {
-  const activeMenu = ref(Number(localStorage.getItem('nav.activeMenu')) || 1)
+  const activeMenu = ref(Number(sessionStorage.getItem('nav.activeMenu')) || 1)
   
   function setActive(menu: number) {
     activeMenu.value = menu
@@ -11,19 +11,19 @@ export const useNavStore = defineStore('nav', () => {
   
   // 清空本地状态
   function clearState() {
-    localStorage.removeItem('nav.activeMenu')
+    sessionStorage.removeItem('nav.activeMenu')
     activeMenu.value = 1 // 重置为默认值
   }
   
-  // 清空所有localStorage
-  function clearAllLocalStorage() {
-    localStorage.clear()
+  // 清空所有sessionStorage
+  function clearAllsessionStorage() {
+    sessionStorage.clear()
     activeMenu.value = 1
   }
   
   watch(activeMenu, (newVal) => {
-    localStorage.setItem('nav.activeMenu', String(newVal))
+    sessionStorage.setItem('nav.activeMenu', String(newVal))
   })
   
-  return { activeMenu, setActive, clearState, clearAllLocalStorage }
+  return { activeMenu, setActive, clearState, clearAllsessionStorage }
 })
