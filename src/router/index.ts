d92f4094b922e6a437b5173bdb140ddcef3303d9
DESKTOP-EB30IMJ\霍青娥 <1520530570@qq.com>
2025-09-27 14:13:04 +0800

    no message

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory  } from 'vue-router'
 
 let routes = [
   {
@@ -15,7 +15,7 @@ let routes = [
   }
 ]
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory (import.meta.env.BASE_URL),
   routes,
 })
 
